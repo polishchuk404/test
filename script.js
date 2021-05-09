9a23c02b65eb83f9b757240701c6fff587f7a9a6
Vadim <polish4uck@gmail.com>
2021-05-09 15:25:22 +0300

    initial project

---
+++ script.js
@@ -0,0 +1,36 @@
+$.getJSON( "../categories.json", function( data ) {
+  var createTable = '';
+  $.each(data.categories, function (i, item) {
+      createTable += '<tr style="height:100px;"><td style="background-color: ' + item.categoryColor + ';">' + item.categoryName + '</td><td style="width:140px" id="drag"></td></tr>';
+  });
+  $('#records_table').append(createTable);
+}).fail(function(){
+    console.log("An error has occurred.");
+});
+$.getJSON( "../data/set1.json", function( data ) {
+  var images = data.images;
+  var createImages = '';
+  $.each(images, function (i, item) {
+    createImages += '<img class="draggable droppable" style="margin:5px" src=" ' + item.imagePath + '" alt="">';
+  });
+  $('#create_images').append(createImages);
+}).fail(function(){
+  console.log("An error has occurred.");
+});
+
+
+$(function() {
+  $('#create_images').sortable({
+      connectWith: '#records_table #drag'
+  });
+  $('#records_table #drag').sortable({
+      connectWith: '#create_images'
+  });
+});
+
+
+
+
+
+
+    
