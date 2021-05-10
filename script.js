24e8bc8af1a58fc1528d00201f9be05caf9d5013
Vadim <polish4uck@gmail.com>
2021-05-10 12:43:22 +0300

    fixed drag

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 $.getJSON( "categories.json", function( data ) {
   var createTable = '';
-  
   $.each(data.categories, function (i, item) {
       createTable += '<tr style="height:100px;"><td style="background-color: ' + item.categoryColor + ';">' + item.categoryName + '</td><td style="width:140px" class="drag"></td></tr>';
   });
   $('#records_table').append(createTable);
+}).done(function() {
+  getImages();
 }).fail(function(){
     console.log("An error has occurred.");
 });
-$.getJSON( "data/set1.json", function( data ) {
-  var images = data.images;
-  var createImages = '';
-  $.each(images, function (i, item) {
-    createImages += '<img class="draggable droppable" style="margin:5px" src=" ' + item.imagePath + '" alt="">';
+
+function getImages() {
+  $.getJSON( "data/set1.json", function( data ) {
+    var createImages = '';
+    $.each(data.images, function (i, item) {
+      createImages += '<img class="draggable droppable" style="display:block; margin:1px" src=" ' + item.imagePath + '" alt="">';
+    });
+    $('#create_images').append(createImages);
+  }).done(function() {
+    dragNdrop();
+  }).fail(function(){
+      console.log("An error has occurred.");
   });
-  $('#create_images').append(createImages);
-}).fail(function(){
-  console.log("An error has occurred.");
-});
-
-
-$(function() {
+};
+function dragNdrop() {
   $('#create_images, #records_table .drag').sortable({
-      connectWith: '#records_table .drag, #create_images'
+    connectWith: '#records_table .drag, #create_images'
   });
-}); 
+};
+
 
 
 
